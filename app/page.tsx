22842c7e3d3394428b84ef86bd0440eb204dab71
Akash Shaw <akash.shaw@baseel.com>
2026-03-27 12:39:54 +0530

    chore: dummy commit to check

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { logo } from "@assets";
 export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center">
-			<Image alt="Sheth Engineering" src={logo} width={200} height={48} priority />
+			<Image alt="Sheth Engineering" src={logo} width={200} height={52} priority />
 		</div>
 	);
 }
